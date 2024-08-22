c36010bbf6bac17fddb112c28967bef04bffbd6a
João Pedro Costa Amaral <116366597+joaopedroBH04@users.noreply.github.com>
2024-08-22 17:59:12 -0300

    Add files via upload

---
+++ SomaNumeros.java
@@ -0,0 +1,13 @@
+public class SomaNumeros {
+    public static void main(String[] args) {
+       
+        int num1 = 58;  
+        int num2 = 42;  
+
+        
+        int soma = num1 + num2;
+
+     
+        System.out.println("A soma de " + num1 + " e " + num2 + " é: " + soma);
+    }
+}
